d6a470be90e896abd4d0a59625c134413233f8cd
Jay-R Doria <jayrdoria876@gmail.com>
2023-09-14 11:54:16 +0800

    Done with the Client Side Validation

--- login_signup_react/src/hooks/useAuthForm.js
+++ login_signup_react/src/hooks/useAuthForm.js
@@ -1,17 +1,76 @@
 import { useState } from 'react';
 
 function useAuthForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState({});
+
+    const resetInputs = () => {
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+    };
+
+    const validateEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return regex.test(email);
+    };
+
     const handleLoginClick = () => {
-        console.log("Login button clicked");
+        let newErrors = {};
+
+        if (!validateEmail(email)) {
+            newErrors.email = "Invalid email format";
+        }
+        if (password.length < 6) {
+            newErrors.password = "Password should be at least 6 characters long";
+        }
+
+        if (Object.keys(newErrors).length === 0) {
+            console.log("Login button clicked");
+        } else {
+            setErrors(newErrors);
+        }
     };
 
     const handleSignUpClick = () => {
-        console.log("Sign Up button clicked");
+        let newErrors = {};
+
+        if (!validateEmail(email)) {
+            newErrors.email = "Invalid email format";
+        }
+        if (password.length < 6) {
+            newErrors.password = "Password should be at least 6 characters long";
+        }
+        if (password !== confirmPassword) {
+            newErrors.confirmPassword = "Passwords do not match";
+        }
+
+        if (Object.keys(newErrors).length === 0) {
+            console.log("Sign Up button clicked");
+        } else {
+            setErrors(newErrors);
+        }
+    };
+
+    const clearErrors = () => {
+        setErrors({});
     };
 
     return {
         handleLoginClick,
-        handleSignUpClick
+        handleSignUpClick,
+        email,
+        setEmail,
+        password,
+        setPassword,
+        confirmPassword,
+        setConfirmPassword,
+        errors,
+        setErrors,
+        clearErrors,
+        resetInputs
     };
 }
 
--- login_signup_react/src/pages/AuthForm.js
+++ login_signup_react/src/pages/AuthForm.js
@@ -3,33 +3,117 @@ import '../css/AuthForm.css';
 import useAuthForm from '../hooks/useAuthForm';
 
 function AuthForm() {
-    const { handleLoginClick, handleSignUpClick } = useAuthForm();
+    const { 
+        handleLoginClick, 
+        handleSignUpClick, 
+        email, 
+        setEmail, 
+        password, 
+        setPassword, 
+        confirmPassword, 
+        setConfirmPassword, 
+        errors,
+        setErrors, // Make sure to destructure setErrors
+        clearErrors,
+        resetInputs
+    } = useAuthForm();
 
     return (
         <div className="container">
-            <input type="checkbox" id="check" />
+            <input 
+                type="checkbox" 
+                id="check" 
+                onChange={() => {
+                    clearErrors();
+                    resetInputs(); // Reset inputs when switching forms
+                }}  
+            />
+
             <div className="login form">
                 <header>Login</header>
                 <form action="#">
-                    <input type="text" placeholder="Enter your Email" />
-                    <input type="password" placeholder="Enter your password" />
+                    <input 
+                        type="text" 
+                        placeholder="Enter your Email" 
+                        value={email} 
+                        onChange={e => {
+                            setEmail(e.target.value);
+                            setErrors(prev => ({ ...prev, email: "" }));
+                        }} 
+                    />
+                    <div className="error">{errors.email}</div>
+
+                    <input 
+                        type="password" 
+                        placeholder="Enter your password" 
+                        value={password} 
+                        onChange={e => {
+                            setPassword(e.target.value);
+                            setErrors(prev => ({ ...prev, password: "" }));
+                        }} 
+                    />
+                    <div className="error">{errors.password}</div>
+
                     <a href="#">Forgot Password?</a>
-                    <input type="button" className="button" value="Login" onClick={handleLoginClick} />
+                    <input 
+                        type="button" 
+                        className="button" 
+                        value="Login" 
+                        onClick={handleLoginClick} 
+                    />
                 </form>
+
                 <div className="signup">
                     <span className="signup">Don't have an account?
                         <label htmlFor="check">Sign Up</label>
                     </span>
                 </div>
             </div>
+
             <div className="registration form">
                 <header>Sign Up</header>
                 <form action="#">
-                    <input type="text" placeholder="Enter your Email" />
-                    <input type="password" placeholder="Create your password" />
-                    <input type="password" placeholder="Confirm your password" />
-                    <input type="button" className="button" value="Sign Up" onClick={handleSignUpClick} />
+                    <input 
+                        type="text" 
+                        placeholder="Enter your Email" 
+                        value={email} 
+                        onChange={e => {
+                            setEmail(e.target.value);
+                            setErrors(prev => ({ ...prev, email: "" }));
+                        }} 
+                    />
+                    <div className="error">{errors.email}</div>
+
+                    <input 
+                        type="password" 
+                        placeholder="Create your password" 
+                        value={password} 
+                        onChange={e => {
+                            setPassword(e.target.value);
+                            setErrors(prev => ({ ...prev, password: "" }));
+                        }} 
+                    />
+                    <div className="error">{errors.password}</div>
+
+                    <input 
+                        type="password" 
+                        placeholder="Confirm your password" 
+                        value={confirmPassword} 
+                        onChange={e => {
+                            setConfirmPassword(e.target.value);
+                            setErrors(prev => ({ ...prev, confirmPassword: "" }));
+                        }} 
+                    />
+                    <div className="error">{errors.confirmPassword}</div>
+
+                    <input 
+                        type="button" 
+                        className="button" 
+                        value="Sign Up" 
+                        onClick={handleSignUpClick} 
+                    />
                 </form>
+
                 <div className="signup">
                     <span className="signup">Already have an account?
                         <label htmlFor="check">Login</label>
