73bf4caa87388add27f9d3cb504a00feca69a444
Jay-R Doria <jayrdoria876@gmail.com>
2023-09-14 21:16:35 +0800

    Changes in Exception: Sign Up

--- login_signup_react/backend/api/login.php
+++ login_signup_react/backend/api/login.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: https://lagueslo.com");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
--- login_signup_react/backend/api/signup.php
+++ login_signup_react/backend/api/signup.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: https://lagueslo.com");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -15,11 +15,15 @@ if(isset($data->email) && isset($data->password)){
     $email = $data->email;
     $password = password_hash($data->password, PASSWORD_DEFAULT);
 
-    $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-    if($stmt->execute([$email, $password])){
-        echo json_encode(["message" => "User registered successfully."]);
-    } else {
-        echo json_encode(["message" => "User registration failed."]);
+    try {
+        $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+        if($stmt->execute([$email, $password])){
+            echo json_encode(["message" => "User registered successfully."]);
+        } else {
+            echo json_encode(["message" => "User registration failed."]);
+        }
+    } catch(PDOException $e) {
+        echo json_encode(["message" => $e->getMessage()]);
     }
 } else {
     echo json_encode(["message" => "Invalid input."]);
